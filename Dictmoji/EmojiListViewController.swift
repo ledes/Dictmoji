aa708f5b63392c41fa0bee335d730809b831be0a
Pablo <pablo.ledesma.p@gmail.com>
2016-01-09 13:23:34 -0500

    List of emojis

---
+++ Dictmoji/EmojiListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiListViewController.swift
+//  Dictmoji
+//
+//  Created by Pablo on 1/9/16.
+//  Copyright © 2016 Pablo Ledesma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmojiListViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel!.text = " 😀 "
+        return cell
+    }
+}
